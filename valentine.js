29cca5e2a54733a56dd4ad7c8f1141054cb18729
Intisar Zaeem <dal659145@utdallas.edu>
2026-02-09 23:17:02 -0600

    Add mouseover and mouseout effects for envelope

---
+++ valentine.js
@@ -0,0 +1,10 @@
+envelope.addEventListener("mouseover", () => {
+  clearTimeout(timeoutId);
+  heartSeal.style.opacity = 0;
+});
+
+envelope.addEventListener("mouseout", () => {
+  timeoutId = setTimeout(() => {
+    heartSeal.style.opacity = 1;
+  }, 1500);
+});
