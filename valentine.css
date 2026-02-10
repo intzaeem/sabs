c1fa52a3c83c81b4d6a71a77d7b4f91279154806
Intisar Zaeem <dal659145@utdallas.edu>
2026-02-09 23:16:45 -0600

    Add CSS styles for card cover and envelope hover effect

---
+++ valentine.css
@@ -0,0 +1,23 @@
+.card-cover {
+  position: absolute;
+  inset: 0;
+  text-align: center;
+  outline: 1px solid #000;
+  transform-style: preserve-3d;
+  transform-origin: left;
+  transition: transform 500ms 0ms;
+}
+
+.envelope:hover .card-cover {
+  transform: rotateY(-135deg);
+  transition-delay: 2250ms;
+}
+
+.card-side {
+  position: absolute;
+  inset: 0;
+  backface-visibility: hidden;
+  background-color: #fff;
+  display: grid;
+  place-items: center;
+}
